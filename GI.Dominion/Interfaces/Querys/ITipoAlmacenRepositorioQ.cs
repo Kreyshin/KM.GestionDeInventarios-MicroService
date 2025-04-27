3d8c660ffb0c1f15a3812891b59111a18a47566d
Kreyshin <kreyshin@gmail.com>
2025-04-27 17:55:47 -0500

    feat(maestros): agregar CRUD para maestro de tipo de Almacen [GI-70]
    - Creacion de entidad e interfaces para repositorios

---
+++ GI.Dominion/Interfaces/Querys/ITipoAlmacenRepositorioQ.cs
@@ -0,0 +1,16 @@
+﻿using GI.Dominio.Comunes;
+using GI.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GI.Dominio.Interfaces.Querys
+{
+    public interface ITipoAlmacenRepositorioQ
+    {
+        public Task<ListResponse<List<TipoAlmacenEN>>> Listar();
+        public Task<SingleResponse<TipoAlmacenEN>> BuscarPorID(string id);
+    }
+}
